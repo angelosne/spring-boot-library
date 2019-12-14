a3757646c5c067c976664dec0bf182567a755474
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:45:36 +0200

    BookshelfResponse update

--- src/main/java/com/example/library/bookshelf/BookshelfResponse.java
+++ src/main/java/com/example/library/bookshelf/BookshelfResponse.java
@@ -1,4 +1,8 @@
 package com.example.library.bookshelf;
 
 public class BookshelfResponse {
+    private long id;
+    private int numberOfBooks;
+    private String country;
+    private boolean isFull;
 }
