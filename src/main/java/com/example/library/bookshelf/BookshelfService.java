ae3486859b7f3be102e7a60923a0df6e7706ba85
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:15:12 +0200

    BookshelfService update

--- src/main/java/com/example/library/bookshelf/BookshelfService.java
+++ src/main/java/com/example/library/bookshelf/BookshelfService.java
@@ -1,4 +1,16 @@
 package com.example.library.bookshelf;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookshelfService {
+    private BookshelfRepository repository;
+    private BookshelfEntityToResponseMapper bookshelfEntityToResponseMapper;
+    private BookshelfInputToEntityMapper bookshelfInputToEntityMapper;
+
+    public BookshelfService(BookshelfRepository repository, BookshelfEntityToResponseMapper bookshelfEntityToResponseMapper, BookshelfInputToEntityMapper bookshelfInputToEntityMapper) {
+        this.repository = repository;
+        this.bookshelfEntityToResponseMapper = bookshelfEntityToResponseMapper;
+        this.bookshelfInputToEntityMapper = bookshelfInputToEntityMapper;
+    }
 }
