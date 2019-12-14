e5c7ea8b9e98a75a6c728c72327971e964a67b8f
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:13:32 +0200

    BookService update

--- src/main/java/com/example/library/book/BookController.java
+++ src/main/java/com/example/library/book/BookController.java
@@ -1,4 +1,7 @@
 package com.example.library.book;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookController {
 }
--- src/main/java/com/example/library/book/BookService.java
+++ src/main/java/com/example/library/book/BookService.java
@@ -1,4 +1,17 @@
 package com.example.library.book;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    private BookRepository repository;
+    private BookEntityToResponseMapper bookEntityToResponseMapper;
+    private BookInputToEntityMapper bookInputToEntityMapper;
+
+    public BookService(BookRepository repository, BookEntityToResponseMapper bookEntityToResponseMapper, BookInputToEntityMapper bookInputToEntityMapper) {
+        this.repository = repository;
+        this.bookEntityToResponseMapper = bookEntityToResponseMapper;
+        this.bookInputToEntityMapper = bookInputToEntityMapper;
+    }
+
 }
