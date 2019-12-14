ebee61378376de8a54fef4dfba414d47a78c8011
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:34:10 +0200

    BookshelfInput update

--- src/main/java/com/example/library/bookshelf/BookshelfInput.java
+++ src/main/java/com/example/library/bookshelf/BookshelfInput.java
@@ -1,4 +1,38 @@
 package com.example.library.bookshelf;
 
 public class BookshelfInput {
+
+    private int numberOfBooks;
+    private String country;
+    private int bookCapacity;
+
+    public BookshelfInput(int numberOfBooks, String country, int bookCapacity) {
+        this.numberOfBooks = numberOfBooks;
+        this.country = country;
+        this.bookCapacity = bookCapacity;
+    }
+
+    public int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+    public void setNumberOfBooks(int numberOfBooks) {
+        this.numberOfBooks = numberOfBooks;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getBookCapacity() {
+        return bookCapacity;
+    }
+
+    public void setBookCapacity(int bookCapacity) {
+        this.bookCapacity = bookCapacity;
+    }
 }
