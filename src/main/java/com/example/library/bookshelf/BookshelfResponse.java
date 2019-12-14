dd953f7cd5468a4e3934832bbaa7842545b8371f
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:48:05 +0200

    BookshelfResponse update

--- src/main/java/com/example/library/bookshelf/BookshelfResponse.java
+++ src/main/java/com/example/library/bookshelf/BookshelfResponse.java
@@ -5,4 +5,43 @@ public class BookshelfResponse {
     private int numberOfBooks;
     private String country;
     private boolean isFull;
+
+    public BookshelfResponse(long id, int numberOfBooks, String country, boolean isFull) {
+        this.id = id;
+        this.numberOfBooks = numberOfBooks;
+        this.country = country;
+        this.isFull = isFull;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+    public void setNumberOfBooks(int numberOfBooks) {
+        this.numberOfBooks = numberOfBooks;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isFull() {
+        return isFull;
+    }
+
+    public void setFull(boolean full) {
+        isFull = full;
+    }
 }
