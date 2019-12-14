894d54c830c2d97fcef4e647d804fe4e55d6e783
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:44:22 +0200

    BookResponse update

--- src/main/java/com/example/library/book/BookResponse.java
+++ src/main/java/com/example/library/book/BookResponse.java
@@ -1,4 +1,81 @@
 package com.example.library.book;
 
+import com.example.library.bookshelf.Bookshelf;
+
+
 public class BookResponse {
+
+    private long id;
+    private String title;
+    private String description;
+    private String authorFullName;
+    private BookGenre genre;
+    private int numberOfPages;
+    private BookSize size;
+
+    public BookResponse(long id, String title, String description, String authorFullName, BookGenre genre, int numberOfPages, BookSize size) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.authorFullName = authorFullName;
+        this.genre = genre;
+        this.numberOfPages = numberOfPages;
+        this.size = size;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthorFullName() {
+        return authorFullName;
+    }
+
+    public void setAuthorFullName(String authorFullName) {
+        this.authorFullName = authorFullName;
+    }
+
+    public BookGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(BookGenre genre) {
+        this.genre = genre;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public BookSize getSize() {
+        return size;
+    }
+
+    public void setSize(BookSize size) {
+        this.size = size;
+    }
 }
---
+++ src/main/java/com/example/library/book/BookSize.java
@@ -0,0 +1,7 @@
+package com.example.library.book;
+
+public enum BookSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
