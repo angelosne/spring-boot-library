30e4fc3ce0748591742af1d7bf2ac1aa7f1465d1
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:16:45 +0200

    BookshelfController update

--- src/main/java/com/example/library/bookshelf/BookshelfController.java
+++ src/main/java/com/example/library/bookshelf/BookshelfController.java
@@ -1,4 +1,12 @@
 package com.example.library.bookshelf;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookshelfController {
+    private BookshelfService service;
+
+    public BookshelfController(BookshelfService service) {
+        this.service = service;
+    }
 }
