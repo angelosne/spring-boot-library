13f2afa153d92833040b8466eba0b631b7fabdd5
Angelosne <negkas.aggelos@gmail.com>
2019-12-14 15:17:08 +0200

    BookController update

--- src/main/java/com/example/library/book/BookController.java
+++ src/main/java/com/example/library/book/BookController.java
@@ -4,4 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+    private BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
 }
